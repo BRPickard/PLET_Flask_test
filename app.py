81406d63f47670bfcc499dcd5dda01d46e321f63
BRPickard <42219723+BRPickard@users.noreply.github.com>
2024-07-18 13:39:54 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import geopandas as gpd
+app = Flask(__name__)
+
+
+@app.route('/result', methods = ['POST', 'GET'])
+
+def result():
+
+    data = request.get_json()
+    
+    for feature in data['features']:
+        feature['properties']['Added Field'] = 'Totally works!'
+    
+    return jsonify(data), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2000)
